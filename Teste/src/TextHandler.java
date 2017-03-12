71d57d7df8af4b520f8cf508ffbdc9680f07d6a2
rmalman <o.rapha@hotmail.com>
2017-03-12 18:39:36 -0300

    Teste

---
+++ Teste/src/TextHandler.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextHandler {
+
+	@Test
+	public void test() {
+		int wordCount = 0;
+		int pipeCount = 0;
+		
+        String s = " 1979   Olá,      meu   nome  é raphael    ";
+        String BuildText = "";
+        boolean word = false;
+        int endOfLine = s.length() - 1;
+        
+        s = s.replaceAll("\\s","|");
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (Character.isLetter(s.charAt(i)) && i != endOfLine) {
+                word = true;     
+                if(pipeCount > 0)
+                {
+                	BuildText += "%"+String.valueOf(pipeCount)+"%";
+                	pipeCount = 0;
+                }
+                BuildText += s.charAt(i); 
+            } else if (!Character.isLetter(s.charAt(i)) && word) {
+                wordCount++;
+                word = false;
+                if (s.charAt(i) != '|')
+                	BuildText += s.charAt(i);
+                if(pipeCount > 0)
+                	BuildText += "%"+String.valueOf(pipeCount)+"%";
+                pipeCount = 0;
+            } else if (Character.isLetter(s.charAt(i)) && i == endOfLine) {
+                wordCount++;
+                if(pipeCount > 0)
+                	BuildText += "%"+String.valueOf(pipeCount)+"%";
+                pipeCount = 0;
+            }
+            if(s.charAt(i) == '|'){
+            	pipeCount++;
+            	System.out.println("Pipecount: [" + String.valueOf(pipeCount) + "] após ["+String.valueOf(wordCount)+"]a. palavra ");
+            	if(i == endOfLine)
+            		BuildText += "%"+String.valueOf(pipeCount)+"%";
+            }
+        }
+        System.out.println("Quantidade de palavras: [" + String.valueOf(wordCount) + "]");
+        System.out.println("Texto Concluido: [" + BuildText + "]");
+
+	}
+
+}
